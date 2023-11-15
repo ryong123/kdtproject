f5d7584688fd71d9ce991d45b8d474dcdce5585a
I00jey <cyj5056@gmail.com>
2023-11-15 13:44:10 +0900

    test

--- cart.js
+++ cart.js
@@ -2,7 +2,7 @@ let label = document.getElementById("label");
 let shoppingCart = document.getElementById("shopping-cart");
 
 let basket = JSON.parse(localStorage.getItem("data")) || [];
-
+// test
 let calculation = () => {
   let cartIcon = document.getElementById("cartAmount");
   cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
