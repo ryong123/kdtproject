19ba9739ee4fc2b4ebd75d4b31c19157ef8425be
I00jey <cyj5056@gmail.com>
2023-11-17 23:00:41 +0900

    main, login

--- javascript/signup.js
+++ javascript/signup.js
@@ -3,11 +3,21 @@ function closeModal() {
     location.href = "http://127.0.0.1:5501/index.html";
 }
 
-function signup() {
-    let id = document.getElementById('id').value;
-    let pw = document.getElementById('pw').value;
-    console.log(id, pw);
-    localStorage.setItem(id,pw)
 
-    location.href = "http://127.0.0.1:5501/index.html";
+function signup() {
+    let id = document.getElementById('id');
+    let pw = document.getElementById('pw');
+
+    if (id.value && pw.value) {
+        console.log(id.value, pw.value);
+        localStorage.setItem(id.value, pw.value)
+        alert('계정이 생성되었습니다.')
+        location.href = "http://127.0.0.1:5501/index.html";
+    } else {
+        alert('아이디와 비밀번호를 입력해주세요.')
+        id.value = "";
+        pw.value = "";
+        return;
+    }
+
 }
