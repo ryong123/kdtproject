ce4ff43ecad2ad31a80e9f79d044b26f6b712bf2
I00jey <cyj5056@gmail.com>
2023-11-19 13:40:47 +0900

    main,signup sweetalert

--- javascript/func.js
+++ javascript/func.js
@@ -1,4 +1,6 @@
 
+
+
 $(window).on("load", function () {
     if (localStorage.getItem('logIn')) {
         $('#loginbtn').css("display", "none");
@@ -24,17 +26,26 @@ function gosignup() {
     location.href = "/html/signup.html";
 }
 
+
+
 function logout() {
+    let idData = localStorage.getItem('logInId');
     localStorage.removeItem('logIn');
-    alert('로그아웃 되었습니다.');
-    location.href = 'index.html';
+    localStorage.removeItem('logInId');
+    swal(`${idData}님`, "로그아웃 되었습니다!", "info")
+        .then((value) => {
+            if (value) {
+                location.href = 'index.html'
+            }
+        });
+    // alert('로그아웃 되었습니다.');
+    // location.href = 'index.html';
 }
 
 function login() {
     const inputId = document.getElementById('loginid');
     const inputPw = document.getElementById('loginpw');
     let success;
-
     if (inputId.value && inputPw.value) {
         if (localStorage.getItem(inputId.value) == inputPw.value) {
             success = true;
@@ -43,7 +54,8 @@ function login() {
             success = false;
         }
     } else {
-        alert('아이디와 비밀번호를 입력해주세요.')
+        swal("입력 오류", "아이디와 비밀번호를 입력해주세요!", "warning");
+        // alert('아이디와 비밀번호를 입력해주세요.')
         inputId.value = "";
         inputPw.value = "";
         return;
@@ -51,12 +63,18 @@ function login() {
 
     if (success) {
         localStorage.setItem('logIn', true);
+        localStorage.setItem('logInId', inputId.value);
+        swal(`${inputId.value}님`, "로그인 되었습니다!", "success")
+            .then((value) => {
+                if (value) {
+                    location.href = 'index.html'
+                }
+            });
         inputId.value = "";
         inputPw.value = "";
-        alert('로그인을 성공했습니다.');
-        location.href = "index.html";
+        // alert('로그인을 성공했습니다.');
     } else {
-        alert('아이디와 비밀번호를 확인해주세요.')
+        swal("입력 오류", "아이디와 비밀번호를 확인해주세요!", "error");
         inputId.value = "";
         inputPw.value = "";
     }
