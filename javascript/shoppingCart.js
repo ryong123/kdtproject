14d5bc0e9ff1e2e7e8bf9fc99251e1c36926b883
sunddaegguk <ssitek22@gmail.com>
2023-11-19 21:39:06 +0900

    final

--- javascript/shoppingCart.js
+++ javascript/shoppingCart.js
@@ -78,7 +78,8 @@ calculation();
 // 클릭 이벤트 처리 함수
 function handleClick1() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=1";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=1";
 }
 // HTML 요소 가져오기
 http: var itemElement = document.getElementById("product-id-a1");
@@ -92,7 +93,8 @@ itemElement.addEventListener("click", handleClick1, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick2() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=2";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=2";
 }
 
 // HTML 요소 가져오기
@@ -107,7 +109,8 @@ itemElement.addEventListener("click", handleClick2, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick3() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=3";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=3";
 }
 
 // HTML 요소 가져오기
@@ -122,7 +125,8 @@ itemElement.addEventListener("click", handleClick3, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick4() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=4";
 }
 
 // HTML 요소 가져오기
@@ -137,7 +141,8 @@ itemElement.addEventListener("click", handleClick4, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick5() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=5";
 }
 
 // HTML 요소 가져오기
@@ -152,7 +157,8 @@ itemElement.addEventListener("click", handleClick5, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick6() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=6";
 }
 
 // HTML 요소 가져오기
@@ -167,7 +173,8 @@ itemElement.addEventListener("click", handleClick6, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick7() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=7";
 }
 
 // HTML 요소 가져오기
@@ -181,7 +188,8 @@ itemElement.addEventListener("click", handleClick7, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick8() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=8";
 }
 
 // HTML 요소 가져오기
@@ -196,7 +204,8 @@ itemElement.addEventListener("click", handleClick8, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick9() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=9";
 }
 
 // HTML 요소 가져오기
@@ -211,7 +220,8 @@ itemElement.addEventListener("click", handleClick9, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick10() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=10";
 }
 
 // HTML 요소 가져오기
@@ -226,7 +236,8 @@ itemElement.addEventListener("click", handleClick10, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick11() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=11";
 }
 
 // HTML 요소 가져오기
@@ -241,7 +252,8 @@ itemElement.addEventListener("click", handleClick11, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick12() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=12";
 }
 
 // HTML 요소 가져오기
@@ -256,7 +268,8 @@ itemElement.addEventListener("click", handleClick12, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick13() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=13";
 }
 
 // HTML 요소 가져오기
@@ -271,7 +284,8 @@ itemElement.addEventListener("click", handleClick13, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick14() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=14";
 }
 
 // HTML 요소 가져오기
@@ -286,7 +300,8 @@ itemElement.addEventListener("click", handleClick14, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick15() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=15";
 }
 
 // HTML 요소 가져오기
@@ -301,7 +316,8 @@ itemElement.addEventListener("click", handleClick15, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick16() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=16";
 }
 
 // HTML 요소 가져오기
@@ -316,7 +332,8 @@ itemElement.addEventListener("click", handleClick16, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick17() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=17";
 }
 
 // HTML 요소 가져오기
@@ -331,7 +348,8 @@ itemElement.addEventListener("click", handleClick17, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick18() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=18";
 }
 
 // HTML 요소 가져오기
@@ -346,7 +364,8 @@ itemElement.addEventListener("click", handleClick18, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick19() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=19";
 }
 
 // HTML 요소 가져오기
@@ -361,7 +380,8 @@ itemElement.addEventListener("click", handleClick19, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick20() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=20";
 }
 
 // HTML 요소 가져오기
@@ -376,7 +396,8 @@ itemElement.addEventListener("click", handleClick20, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick21() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=21";
 }
 
 // HTML 요소 가져오기
@@ -391,7 +412,8 @@ itemElement.addEventListener("click", handleClick21, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick22() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=22";
 }
 
 // HTML 요소 가져오기
@@ -406,7 +428,8 @@ itemElement.addEventListener("click", handleClick22, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick23() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=23";
 }
 
 // HTML 요소 가져오기
@@ -421,7 +444,8 @@ itemElement.addEventListener("click", handleClick23, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick24() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=24";
 }
 
 // HTML 요소 가져오기
@@ -436,7 +460,8 @@ itemElement.addEventListener("click", handleClick24, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick25() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=25";
 }
 
 // HTML 요소 가져오기
@@ -451,7 +476,8 @@ itemElement.addEventListener("click", handleClick25, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick26() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=26";
 }
 
 // HTML 요소 가져오기
@@ -466,7 +492,8 @@ itemElement.addEventListener("click", handleClick26, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick27() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=27";
 }
 
 // HTML 요소 가져오기
@@ -481,7 +508,8 @@ itemElement.addEventListener("click", handleClick27, { once: true });
 // 클릭 이벤트 처리 함수
 function handleClick28() {
   // 특정 URL로 이동
-  window.location.href = "//127.0.0.1:5501/HTML/detail.html?id=4";
+  window.location.href =
+    "https://frabjous-longma-abd4cf.netlify.app/HTML/detail.html?id=28";
 }
 
 // HTML 요소 가져오기
